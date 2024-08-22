0f178c0107a958254aa7dfb93f9cc9f01e3a7f82
Otoniel  Yone <otonielyone@gmail.com>
2024-08-22 10:51:59 -0400

    dealijg with venv folders

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ app = create_app()
 
 #if __name__ == "__main__":
 #    import uvicorn
+<<<<<<< HEAD
 #    uvicorn.run(app, host="127.0.0.1", port=5000)
 
 
@@ -12,3 +13,6 @@ app = create_app()
 #netstat -tuln | grep LISTEN
 #lsof -i :5000
 #kill -9 282229
+=======
+#    uvicorn.run(app, host="127.0.0.1", port=50000)
+>>>>>>> 5531c79 (cleaning files)
