ec7406fdb35c1d809ab2355b3c4074af1792195d
Otoniel  Yone <otonielyone@gmail.com>
2024-08-23 00:02:39 -0400

    chaging structure

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ app = create_app()
 #if __name__ == "__main__":
 #    import uvicorn
 #    uvicorn.run(app, host="127.0.0.1", port=5000)
-
-
-#gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:5000
-#kill blocking ports
-#netstat -tuln | grep LISTEN
-#lsof -i :5000
-#kill -9 282229
-
-#    uvicorn.run(app, host="127.0.0.1", port=50000)
+#    gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:5000
+#   netstat -tuln | grep LISTEN
+#   lsof -i :5000
+#   kill -9 282229
